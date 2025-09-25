cc300c845a5ac5717a00c17154c2b468d9457834
AkankshaG123 <akanksha.gawade@emergys.com>
2025-09-25 14:36:31 +0530

    PATCH : added Sidebar

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+const Mainlayout = () => {
+  return (
+    <div className="bg-[#F5FAFF] h-screen flex flex-row overflow-hidden">
+        <Sidebar/>
+    </div>
+  )
+}
+
+export default Mainlayout
