9d11563577b37adf77ea65374f617559589eefe0
Sayali Salunke <sayali.salunke@emergys.com>
2025-09-29 11:58:03 +0530

    PATCH : changes in mainLayour

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router";
 import UploadPage from "./pages/UploadPage";
 import ReviewExtracted from "./pages/ReviewExtracted";
-import MainLayout from "./Layout/MainLayout";
+import Mainlayout from "./Layout/Mainlayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout/>,
+    element: <Mainlayout/>,
     children:[
         {path:"/upload",element:<UploadPage/>},
         {path:"/reviewExtracted",element:<ReviewExtracted/>}
